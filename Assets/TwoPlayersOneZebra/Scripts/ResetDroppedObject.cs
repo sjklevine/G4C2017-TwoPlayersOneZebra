04b8bec7a3006eeb11cb1fcd0d26eb4d31d9630a
Bracey <bracey@potentialsynergy.com>
2017-07-29 16:26:39 -0400

    Script to reset dropped objects

---
+++ Assets/TwoPlayersOneZebra/Scripts/ResetDroppedObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetDroppedObject : MonoBehaviour {
+	private Vector3 startPosition;
+	private Quaternion startRotation;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <= (startPosition.y - 1)) {
+			transform.position = startPosition;
+			transform.rotation = startRotation;
+		}
+	}
+}
