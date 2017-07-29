d5a98207f75bdeea12c25b517b0fa6a3a851ec48
Bracey <bracey@potentialsynergy.com>
2017-07-29 16:05:30 -0400

    Added pick-upabble toothbrush and glass with interaction collision!

---
+++ Assets/TwoPlayersOneZebra/Scripts/ToothbrushZoneScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothbrushZoneScript : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Toothbrush")) {
+			Debug.Log ("YEAHHHHHH");
+		}
+	}
+}
