47837fd80db3487482a83f7afadcf92b5d329ace
Sam Levine <samuel.levine@gmail.com>
2017-07-29 17:06:04 -0400

    Toothpaste!  Might not work, but need to test it.  Plus prefabs for our major objects.

---
+++ Assets/TwoPlayersOneZebra/Scripts/CustomToothpasteGrabAction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using VRTK;
+using VRTK.SecondaryControllerGrabActions;
+
+public class CustomToothpasteGrabAction : VRTK_SwapControllerGrabAction
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+        isSwappable = false; // Because second hand grab means TOOTHPASTE
+    }
+
+    /*
+    public override void Initialise(VRTK_InteractableObject currentGrabbdObject, VRTK_InteractGrab currentPrimaryGrabbingObject, VRTK_InteractGrab currentSecondaryGrabbingObject, Transform primaryGrabPoint, Transform secondaryGrabPoint)
+    {
+        // On secondary grab, PICK UP THE CAP
+
+        // Also restore this to be swappable
+    }
+    */
+
+}
---
+++ Assets/TwoPlayersOneZebra/Scripts/InteractableToothpasteScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class InteractableToothpasteScript : VRTK_InteractableObject
+{
+    public VRTK_InteractableObject toothpasteCap;
+
+    public override bool PerformSecondaryAction()
+    {
+        if (base.PerformSecondaryAction())
+        {
+            Debug.Log("I'M GETTING GRABBED BY HAND 2");
+            Debug.Log("GRABBING OBJECT = " + GetGrabbingObject().name);
+            Debug.Log("2nd GRABBING OBJECT = " + GetSecondaryGrabbingObject().name);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
