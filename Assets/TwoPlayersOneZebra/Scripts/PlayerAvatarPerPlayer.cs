409f34fc7e7cda6222fb4b57448f5e1bdf6d944d
Howard Olah-Reiken <howard@preferredapp.com>
2017-07-30 13:01:14 -0400

    for head sync

---
+++ Assets/TwoPlayersOneZebra/Scripts/PlayerAvatarPerPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAvatarPerPlayer : MonoBehaviour {
+
+	public GameObject leftHand;
+	public GameObject rightHand;
+	public GameObject head;
+
+	private int playerNumber;
+
+	public void SetPlayerNumber(int playerNumber) {
+		this.playerNumber = playerNumber;
+		if (playerNumber == 0) {
+			leftHand.SetActive (false);
+		} else {
+			rightHand.SetActive (false);
+		}
+
+		PhotonView photonView = GetComponent<PhotonView> ();
+		if (!photonView.isMine) {
+			head.SetActive (false);
+		}
+	}
+}
