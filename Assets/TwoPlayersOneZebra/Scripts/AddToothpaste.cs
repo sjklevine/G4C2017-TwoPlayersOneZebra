7ef927e75822f63cb1d9fb63829126ce13a0c06e
Bracey <bracey@potentialsynergy.com>
2017-07-29 20:48:14 -0400

    End of day commit!  Toothpaste tube/cap/brush prefab updates, merged Bath + Bed into Main, probably more

--- Assets/TwoPlayersOneZebra/Scripts/AddToothpaste.cs
+++ Assets/TwoPlayersOneZebra/Scripts/AddToothpaste.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class AddToothpaste : MonoBehaviour {
 	
 	public GameObject toothpaste;
+	public GameObject cap;
+	private bool capOff;
 
 	void Start (){
-		//toothpaste.SetActive (false);
+		toothpaste.SetActive (false);
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag.Equals ("Tube")) {
+
+		capOff = cap.GetComponent<InteractableToothpasteScript> ().isUsable;
+		if (other.gameObject.tag.Equals ("Tube") && capOff == true) {
 			toothpaste.SetActive(true);
 		}
 	}
--- Assets/TwoPlayersOneZebra/Scripts/BrushingTeeth.cs
+++ Assets/TwoPlayersOneZebra/Scripts/BrushingTeeth.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BrushingTeeth : MonoBehaviour {
+	public ParticleSystem foam;
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag.Equals ("Teeth")) {
 			Debug.Log ("brush brush brush");
+			if (foam != null) {
+				foam.Play ();
+			}
 		}
+	}
+		void OnTriggerExit(Collider other){
+			if (other.gameObject.tag.Equals ("Teeth")) {
+			if (foam != null) {
+				foam.Stop ();
+			}
+			}
+		
 	}
 }
